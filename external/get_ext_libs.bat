43ad54c314c7ca931ea7b6c1cafb9c4c77d53ae2
Murat Cakmak <murat@za-ya.co>
2020-08-03 23:38:16 +0300

    mbed-crypto library

---
+++ external/get_ext_libs.bat
@@ -0,0 +1,1 @@
+git clone https://github.com/ARMmbed/mbed-crypto.git
--- source/main.c
+++ source/main.c
@@ -4,6 +4,12 @@
 /* Remove the include */
 #include <windows.h> // only runs on Windows
 
+#include "aes.h"
+
+#define EXAMPLE_AES_KEY_LEN_IN_BITS		256
+#define EXAMPLE_AESKEY_LEN_IN_BYTES		(EXAMPLE_AES_KEY_LEN_IN_BITS / 8)
+#define AES_IV_LENGTH					(16)
+
 extern int Boot_Log(char* format, ...);
 
 int main(void) {
@@ -11,6 +17,26 @@ int main(void) {
 	Sleep(100);
 
 	Boot_Log("Hello World!");
+
+	mbedtls_aes_context aes;
+
+	unsigned char key[EXAMPLE_AESKEY_LEN_IN_BYTES];
+	/* TODO create an AES Key ; Just set random values here */
+
+
+	unsigned char iv[AES_IV_LENGTH];
+	/* TODO create an IV ; Just set random values here */
+
+	unsigned char input[16] = "ZAYA Internship";
+	unsigned char output[16 + 1];
+
+	mbedtls_aes_setkey_enc(&aes, key, EXAMPLE_AES_KEY_LEN_IN_BITS);
+
+	mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_ENCRYPT, 16, iv, input, output);
+	output[16] = '\0'; /* Let us terminate the cipher text */
+
+	Boot_Log("The Plain Text   : %s", input);
+	Boot_Log("The Cipher Text  : %s", output);
 
 	int cnt = 0;
 	while (1) {
