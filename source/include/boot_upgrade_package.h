1cf67c3e64ba2515e3828bfcedc43457bb4fcc7b
asena <asenakirik@gmail.com>
2020-08-12 15:34:33 +0300

    T-11: Upgrade Package Structure

--- source/include/boot_upgrade_package.h
+++ source/include/boot_upgrade_package.h
@@ -7,12 +7,16 @@
 typedef struct
 {
     uint32_t dummy;
-    /* TODO all fields here */
+    uint8_t prefix[4];
+    uint32_t version;
+    uint32_t size;
+    uint8_t signature[64]
 } boot_image_metadata_t;
 
 typedef struct
 {
     boot_image_metadata_t metadata;
+    uint32_t image;
     /* TODO image here */
 } boot_upgrade_package_t;
 
