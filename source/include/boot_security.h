fe675a65b4d61332bee49a5ba04b89b0fc57b86c
asenaasena <62607543+asenaasena@users.noreply.github.com>
2020-08-17 13:42:42 +0300

    T-11 : Upgrade Package Structure

--- source/include/boot_security.h
+++ source/include/boot_security.h
@@ -3,6 +3,7 @@
 #define __BOOT_SECURITY_H
 
 #include "boot_upgrade_package.h"
+#include "boot_types.h"
 
 bool boot_authenticate_upgrade_package(boot_upgrade_package_t* package);
 
--- source/include/boot_upgrade_package.h
+++ source/include/boot_upgrade_package.h
@@ -1,19 +1,36 @@
-
 #ifndef __UPGRADE_PACKAGE_H
 #define __UPGRADE_PACKAGE_H
 
 #include "boot_types.h"
 
+/*
+ * Upgrade Package Metadata
+ */
+
 typedef struct
 {
-    uint32_t dummy;
-    /* TODO all fields here */
+
+    /* Upgrade Package Metadata Prefix : Must be ZAYA */
+    uint8_t prefix[4];
+    /* Version of the image/application */
+    uint32_t version;
+    /* Image size */
+    uint32_t size;
+    uint8_t reserved[4];
+    /* Decryption initialization vector*/
+    uint8_t iv[16];
+    /* Signature */
+    uint8_t signature[128];
 } boot_image_metadata_t;
 
 typedef struct
 {
+    /* Metadata */
     boot_image_metadata_t metadata;
+    /* Application */
+    uint8_t image[1];
     /* TODO image here */
+
 } boot_upgrade_package_t;
 
 #endif
