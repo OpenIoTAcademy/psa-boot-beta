637fe3e7ad89b9f1f8c31f2ebbc95ea8cf6a26f1
ozdemirfadime <fadimeozdemir228@gmail.com>
2020-08-17 20:57:20 +0300

    uupdated boot_decrypt_upgrade_package with structure

--- source/boot_security.c
+++ source/boot_security.c
@@ -13,10 +13,9 @@ void boot_decrypt_upgrade_package(boot_upgrade_package_t* package)
 {
     mbedtls_aes_context aes;
     unsigned char key[EXAMPLE_AESKEY_LEN_IN_BYTES];
-    uint8_t* input = package->metadata.image;
+    uint8_t* input = package->image;
     uint8_t* output = input;
-    unsigned char iv[16] = "aa";
-    LOG_PRINTF("The iv Text  : %s", iv);
+   
     mbedtls_aes_setkey_dec(&aes, key, EXAMPLE_AES_KEY_LEN_IN_BITS);
-    mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, package->metadata.size, iv, input, output);
+    mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, package->metadata.size, package->metadata.iv, input, output);
 }
